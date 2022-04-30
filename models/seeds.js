b107c540e069102a5e7319cf3f5d3fc4a6812f78
Martin Yanchev <myanch200@gmail.com>
2022-04-30 22:53:56 +0100

    adding the entries

--- models/seeds.js
+++ models/seeds.js
@@ -5,14 +5,14 @@ exports.dishes = [
     name: 'Pizza Margherita',
     description: 'Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven.',
     price: '£10.00',
-    image: 'pizza.jpg' ,
+    image: 'pizza-2.jpeg' ,
     votes: 1,
   },
   {
     name: "Pizza Pepperoni",
     description: 'Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (anchovies, olives, meat, etc.) baked at a high temperature, traditionally in a wood-fired oven.',
     price: '£12.00',
-    image: 'pizza.jpg',
+    image: 'pizza.jpeg',
     votes: 2,
   }
 ]
